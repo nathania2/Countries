dc0988c4a386d1287a745d8f146e6741b9465cad
nathania2 <mahiderdawit6@gmail.com>
2025-01-23 13:27:41 +0000

    map

--- workspace/Main.java
+++ workspace/Main.java
@@ -1,3 +1,6 @@
+//Name: Nathania Dawit 
+//Date: 1/23/2025
+// Description: this program displays county data and image allowing users to answer map quiz questions.
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -40,51 +43,65 @@ public class Main
     // Write a for loop that goes through the countryArray.
     // for(int i ....) {
     // Do the following inside the loop
+    for (int i = 0; i < countryArray.length; i++) {
+      if (!scan.hasNextLine()) break; 
       String input = scan.nextLine();
       String[] data = input.split(",");
       System.out.println("Read in " + data[0]);
+      countryArray[i] = new Country(data[0], data[1], data[2], data[3]);
+    }
+    scan.close();
+}
       // inside the loop, create a new Country using your constructor with 4 arguments and pass in data[0], data[1], data[2], data[3] as arguments.
      // inside the loop, set countryArray[i] to the created Country object
      
-    
-  }
+  
 
   /* showCountry() will show the image associated with the current country. It should get the country at index from the countryArray. It should use its get method to get its image file name and use the code below to put the image in the GUI.
   */
   public void showCountry() {
     // Get the country at index from countryArray
-    
+    if (countryArray[index] != null) {
     // Use its get method to get the its image file name and save it into imagefile variable below instead of worldmap.jpg.
-    String imagefile = "worldmap.jpg";
+    String imagefile = countryArray[index].getImageFile();
     // Use the following code to create an new Image Icon and put it into the GUI
-    img = new ImageIcon("/workspaces/Countries/workspace/"+imagefile);
+    img = new ImageIcon("/workspaces/Countries/workspace/" + imagefile);
     imageLabel.setIcon(img);
   }
-  
+}
   /* nextButton should increment index. If the index is greater than 9, reset it back to 0. Clear the outputLabel to empty string using setText, and call showCountry();*/
-  public void nextButtonClick()
-  {
-    
-  }
-  
+  public void nextButtonClick() {
+    index = (index + 1) % countryArray.length; 
+    outputLabel.setText(""); 
+    showCountry(); 
+}
   /* reviewButton should get the country at index from the countryArray, call its toString() method and save the result, print it out with System.out.println and as an argument to outputLabel.setText( text to print out ); */
-  public void reviewButtonClick()
-  {
-     
-  }
+  public void reviewButtonClick() {
+    if (countryArray[index] != null) {
+        String details = countryArray[index].toString();
+        System.out.println(details);
+        outputLabel.setText(details); 
+    }
+}
 
   /* quizButton should clear the outputLabel (outputLabel.setText to empty string), get the country at index from countryArray, print out a question about it like What country is this? and/or What's this country's capital?. Get the user's answer using scan.nextLine() and check if it is equal to the country's data using its get methods and print out correct or incorrect.
   */
-  public void quizButtonClick()
-  {
-    Scanner scan = new Scanner(System.in); 
-    
-    
-    
-  }
-
-
-
+  public void quizButtonClick() {
+    if (countryArray[index] != null) {
+        Scanner scan = new Scanner(System.in);
+        Country currentCountry = countryArray[index];
+        // Ask a question about the country's name or capital
+        System.out.println("What country is this?");
+        String answer = scan.nextLine();
+        if (answer.equalsIgnoreCase(currentCountry.getName())) {
+            System.out.println("Correct!");
+            outputLabel.setText("Correct!");
+        } else {
+            System.out.println("Incorrect! The correct answer is " + currentCountry.getName());
+            outputLabel.setText("Incorrect! The correct answer is " + currentCountry.getName());
+        }
+    }
+}
 
   /* Do NOT change anything below here */
   /* The Main() constructor is finished and will construct the GUI */
